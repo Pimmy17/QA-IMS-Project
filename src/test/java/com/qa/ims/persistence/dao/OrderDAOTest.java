c9bbf1c572f590d3c31213f162c88d90e7269cb0
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-07-08 14:46:10 +0100

    Starting creation of orders

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -1,5 +1,22 @@
 package com.qa.ims.persistence.dao;
 
+import org.junit.Before;
+
+import com.qa.ims.utils.DBUtils;
+
 public class OrderDAOTest {
+
+	private final OrderDAO DAO = new OrderDAO();
+
+	@Before
+	public void setup() {
+		DBUtils.connect();
+		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+	}
+
+	@Test
+	public void testCreate() {
+		final Order created = new Order
+	}
 
 }
