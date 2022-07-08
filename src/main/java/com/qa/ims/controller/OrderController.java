024aaeb19330f6b69f96d73cd5bbcacc1805cfe8
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-07-08 15:33:51 +0100

    Create order fields being defined

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,5 +1,83 @@
 package com.qa.ims.controller;
 
-public class OrderController {
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private OrderDAO orderDAO;
+	private Utils utils;
+
+	public OrderController(OrderDAO orderDAO, Utils utils) {
+		super();
+		this.orderDAO = orderDAO;
+		this.utils = utils;
+	}
+
+	/**
+	 * Reads all orders to the logger
+	 */
+	@Override
+	public List<Order> readAll() {
+		List<Order> orders = orderDAO.readAll();
+		for (Order order : orders) {
+			LOGGER.info(order);
+		}
+		return orders;
+	}
+
+	/**
+	 * Creates an order by taking in user input
+	 */
+	@Override
+	public Order create() {
+		LOGGER.info("Please select a customer");
+		// Create a drop-down menu to select an existing customer
+		Long customerID = utils.getLong();
+		LOGGER.info("Please add an item to the order");
+		// Create a drop-down menu to select an existing item
+		Long itemID = utils.getLong();
+		LOGGER.info("Please enter a quantity");
+		Integer quantity = utils.getInt();
+		Order order = orderDAO.create(new Order(customerID, itemID, quantity));
+		LOGGER.info("Order created");
+		return order;
+	}
+
+	/**
+	 * Updates an existing order by taking in user input
+	 */
+	@Override
+	public Order update() {
+		LOGGER.info("Please enter the id of the order you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter an item name");
+		// Create a drop-down menu of current items in the order
+		Long itemID = utils.getLong();
+		LOGGER.info("Please enter a quantity");
+		Integer quantity = utils.getInt();
+		Order order = orderDAO.update(new Order(id, itemID, quantity));
+		LOGGER.info("Order Updated");
+		return order;
+	}
+
+	/**
+	 * Deletes an existing order by the id of the order
+	 * 
+	 * @return
+	 */
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long id = utils.getLong();
+		return orderDAO.delete(id);
+	}
 
 }
--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -1,5 +1,27 @@
 package com.qa.ims.persistence.dao;
 
-public class OrderDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+
+public class OrderDAO implements Dao<Order> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("id");
+		Long customerID = resultSet.getLong("customerID");
+		Long itemID = resultSet.getLong("itemID");
+		Integer quantity = resultSet.getInt("quantity");
+		return new Order(id, customerID, itemID, quantity);
+	}
+
+	/**
+	 * Reads all items from the database
+	 * 
+	 * @return A list of items
+	 */
 
 }
