6eff491fa663ce25e1c98e36d2c1a470b8f6a72e
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-07-13 12:57:13 +0100

    Added testing for orders create and read all

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,2 +1,3 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `items` (`item_name`, `price`) VALUES ('Bop-it', 5.99);
+INSERT INTO `orders_items` (`orders_id`, `items_id`, `quantity`) VALUES (1, 1, 2);
